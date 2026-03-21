3bd415b946e02f121a66b9a1fbdc90f855bb597a
M K Samuel Joseph <mksamjoseph29@gmail.com>
2026-03-21 11:46:04 +0530

    first upload

--- hospital_system/settings.py
+++ hospital_system/settings.py
@@ -121,9 +121,15 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-static_dir = BASE_DIR / 'static'
-STATICFILES_DIRS = [static_dir]
+import os
+
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
